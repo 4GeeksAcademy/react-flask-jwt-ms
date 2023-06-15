1cec961c7faab1ed71d5b13f7c96ebd978130469
Manuel Salazar <121062158+manolos05@users.noreply.github.com>
2023-06-15 01:32:59 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -7,10 +7,13 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
-from api.models import db
+from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+import jwt
 
 #from models import Person
 
@@ -47,6 +50,19 @@ app.register_blueprint(api, url_prefix='/api')
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
+#Function to encode a token
+def encode_auth_token(user_id):
+    try:
+        payload ={
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            'iat': datetime.datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(payload, app.config['JWT_SECRET_KEY'], algorithm='HS256')
+    except Exception as e:
+        return e
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
@@ -62,6 +78,30 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
+
+@app.route('/user', methods=['GET'])
+def handle_user():
+    users = User.query.all() 
+    return jsonify([use.serialize() for use in users])
+
+@app.route('/signup', methods=['POST'])
+def handle_signup():
+
+   data = request.get_json()
+
+   user = User.query.filter_by(email=data['email']).first()
+   if user:
+       return jsonify(message= 'user alredy exist'), 400
+   
+   
+  
+   new_user = User(email=data['email'], password= pwd_hash, is_active=True)
+   db.session.add(new_user)
+   db.session.commit()
+
+   aut_token = encode_auth_token(new_user.id)
+   return jsonify(aut_token = aut_token), 201
+
 
 
 # this only runs if `$ python src/main.py` is executed
