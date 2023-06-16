d6a45d84a55ae863c8a8c7ca3a40fbbea76b0ab3
Manuel Salazar <121062158+manolos05@users.noreply.github.com>
2023-06-16 00:25:29 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ ENV = os.getenv("FLASK_ENV")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['JWT_SECRET_KEY']= 'myjwtsecretkey'
+app.config['JWT_SECRET_KEY']= 'holabbquemaspues'
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -63,6 +63,17 @@ def encode_auth_token(user_id):
     except Exception as e:
         return e
 
+#Function to decode a token
+def decode_auth_token(auth_token):
+    try:
+        payload = jwt.decode(payload, app.config['JWT_SECRET_KEY'], algorithm='HS256')
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        return 'Token expired. Please log again'
+    except jwt.InvalidTokenError:
+        return 'Invalid token. Please log again'
+    
+    
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
@@ -118,6 +129,46 @@ def handle_login():
    else:
        return jsonify(message='Wrong credentials'), 401
    
+@app.route('/validate_token', methods=['POST'])
+def validate_token():
+    data = request.get_json()
+    token = data['token']
+
+    try:
+
+        payload = jwt.decode(token, app.config['JWT_SECRET_KEY'])
+
+        user = User.query.filter_by(id=payload['sub']).first()
+        if user:
+            return jsonify(valid=True, user=user.serialize())
+        else:
+            return jsonify(valid=False, message='User not found')
+    except jwt.ExpiredSignatureError:
+        return jsonify(valid=False, message='Token has expired')
+    except jwt.InvalidTokenError:
+        return jsonify(valid=False, message='Invalid Token')
+
+@app.route('/dashboard', mehtods=['GET'])
+def dashboard():
+    
+    auth_header= request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        return jsonify(message= 'Token is missing'), 401
+    
+    user_id = decode_auth_token(auth_token)
+    if isinstance(user_id, str):
+        return jsonify(message=user_id), 401
+    
+    user= User.query.filter_by(id=user_id).first()
+    if not user:
+        return jsonify(message= 'User not found'), 404
+
+    return jsonify(message='Welcome to your dashboard!', user=user.serializa())
+    
+    
+
     
 
 
